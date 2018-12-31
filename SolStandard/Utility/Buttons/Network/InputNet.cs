425c60374a0f1b63b58e3421898fba545c127c90
Talberon <atheion@outlook.com>
2018-12-31 11:26:55 -0400

    Removed unnecessary serialization for InputNet

--- SolStandard/Utility/Buttons/Network/InputNet.cs
+++ SolStandard/Utility/Buttons/Network/InputNet.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Runtime.Serialization;
 using Microsoft.Xna.Framework;
-using SharpDX.Direct3D9;
 using SolStandard.Utility.Buttons.Gamepad;
 
 namespace SolStandard.Utility.Buttons.Network
 {
-    [Serializable]
-    public class InputNet : GameControl, ISerializable
+    public class InputNet : GameControl
     {
         public const string PlayerIndexHeader = "PlayerIndex";
 
@@ -22,12 +18,6 @@ namespace SolStandard.Utility.Buttons.Network
         public InputNet(PlayerIndex playerIndex) : base(playerIndex)
         {
             pressed = false;
-        }
-
-        public InputNet(SerializationInfo info, StreamingContext context) :
-            this((PlayerIndex) info.GetValue(PlayerIndexHeader, typeof(PlayerIndex)))
-        {
-            pressed = (bool) info.GetValue(ControlState.Pressed.ToString(), typeof(bool));
         }
 
         public override bool Pressed
@@ -48,13 +38,6 @@ namespace SolStandard.Utility.Buttons.Network
         public void Release()
         {
             pressed = false;
-        }
-
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue(PlayerIndexHeader, PlayerIndex);
-            info.AddValue(ControlState.Pressed.ToString(), Pressed);
-            info.AddValue(ControlState.Released.ToString(), Released);
         }
 
         public override string ToString()
