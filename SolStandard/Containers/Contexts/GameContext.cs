50867de64e1dd6652050c428b17a36fcf32cf740
Talberon <atheion@outlook.com>
2019-01-19 19:41:50 -0400

    New Map Select Screen. Adjusted existing maps.

--- SolStandard/Containers/Contexts/GameContext.cs
+++ SolStandard/Containers/Contexts/GameContext.cs
@@ -32,7 +32,7 @@ namespace SolStandard.Containers.Contexts
         public static readonly Color NeutralColor = new Color(255, 250, 250);
 
         private const string MapDirectory = "Content/TmxMaps/";
-        private const string MapSelectFile = "Map_Select_02.tmx";
+        private const string MapSelectFile = "Map_Select_03.tmx";
 
         public static BattleContext BattleContext { get; private set; }
         public static Scenario Scenario { get; private set; }
