d903ed9504eb674a81b78bbb6e62c524adcb89a0
Talberon <atheion@outlook.com>
2019-03-30 14:16:26 -0300

    Initial page routing

--- sol-standard-web/src/app/app.module.ts
+++ sol-standard-web/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MediaComponent } from './media/media.component';
+import { DownloadComponent } from './download/download.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'media', component: MediaComponent },
+  { path: 'download', component: DownloadComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MediaComponent,
+    DownloadComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    UiModule
+    UiModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ sol-standard-web/src/app/media/media.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media',
+  templateUrl: './media.component.html',
+  styleUrls: ['./media.component.less']
+})
+export class MediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
