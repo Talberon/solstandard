d14e828e412d0df2ea04a861d667f87079e7444e
Talberon <atheion@outlook.com>
2019-05-23 07:51:08 -0300

    Added threshold for cached generated unit names

--- SolStandard/Entity/Unit/NameGenerator.cs
+++ SolStandard/Entity/Unit/NameGenerator.cs
@@ -5,6 +5,7 @@ namespace SolStandard.Entity.Unit
 {
     public static class NameGenerator
     {
+        private const int MaxCachedNames = 50;
         private static List<string> _usedNames;
 
         private static List<string> UsedNames
@@ -76,6 +77,10 @@ namespace SolStandard.Entity.Unit
         private static string GenerateName(NameType nameType)
         {
             List<string> nameList = FetchNameList(nameType);
+
+            //Name generation should not break because too many units have spawned with generated names.
+            //Clear the cache after a significant number of units have spawned.
+            if (UsedNames.Count > MaxCachedNames) ClearNameHistory();
 
             while (true)
             {
