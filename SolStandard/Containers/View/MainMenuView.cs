8a38abe70998867807fa874a44b87cfb90e401ac
Talberon <atheion@outlook.com>
2019-11-22 18:38:38 -0400

    Added info for whether gamepads are connected on main menu

--- SolStandard/Containers/View/MainMenuView.cs
+++ SolStandard/Containers/View/MainMenuView.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using SolStandard.HUD.Menu;
 using SolStandard.HUD.Menu.Options;
 using SolStandard.HUD.Menu.Options.MainMenu;
+using SolStandard.HUD.Window;
 using SolStandard.HUD.Window.Content;
 using SolStandard.Utility;
 using SolStandard.Utility.Assets;
@@ -15,12 +17,15 @@ namespace SolStandard.Containers.View
         private readonly IRenderable title;
         private readonly IRenderable logo;
         private readonly IRenderable copyright;
+        private IRenderable gamepadStatusWindow;
+        private string gamepadStateCache;
 
         public MainMenuView(IRenderable title, IRenderable logo)
         {
             this.title = title;
             this.logo = logo;
             MainMenu = GenerateMainMenu();
+
             copyright = new RenderText(AssetManager.WindowFont, "Copyright @Talberon 2019",
                 new Color(100, 100, 100, 100));
         }
@@ -59,6 +64,7 @@ namespace SolStandard.Containers.View
             DrawMenu(spriteBatch, centerScreen, titlePosition);
 
             copyright.Draw(spriteBatch, GameDriver.ScreenSize - new Vector2(copyright.Width, copyright.Height));
+            GamepadStatusWindow.Draw(spriteBatch, new Vector2(0, GameDriver.ScreenSize.Y - GamepadStatusWindow.Height));
         }
 
         private void DrawMenu(SpriteBatch spriteBatch, Vector2 centerScreen, Vector2 titlePosition)
@@ -69,5 +75,23 @@ namespace SolStandard.Containers.View
                 new Vector2(centerScreen.X - mainMenuCenter.X, titlePosition.Y + title.Height + titlePadding);
             MainMenu.Draw(spriteBatch, mainMenuPosition);
         }
+
+        private IRenderable GamepadStatusWindow
+        {
+            get
+            {
+                if (gamepadStateCache == CurrentGamepadStates) return gamepadStatusWindow;
+                gamepadStateCache = CurrentGamepadStates;
+                gamepadStatusWindow = new Window(new RenderText(AssetManager.WindowFont, gamepadStateCache),
+                    MenuColor);
+                return gamepadStatusWindow;
+            }
+        }
+
+        private static string CurrentGamepadStates =>
+            $"P1 Gamepad Detected: {GamePad.GetCapabilities(PlayerIndex.One).IsConnected}\n" +
+            $"P2 Gamepad Detected: {GamePad.GetCapabilities(PlayerIndex.Two).IsConnected}\n" +
+            $"Joystick 1 Detected: {Joystick.GetCapabilities(0).IsConnected}\n" +
+            $"Joystick 2 Detected: {Joystick.GetCapabilities(1).IsConnected}";
     }
 }
