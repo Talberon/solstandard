1c34f11b458c9e8351a9f490048d560527860b2f
Talberon <atheion@outlook.com>
2020-08-11 21:12:57 -0300

    Fixed toggle fullscreen option

--- SolStandard/HUD/Menu/Options/PauseMenu/ConfigMenu/ToggleFullscreenOption.cs
+++ SolStandard/HUD/Menu/Options/PauseMenu/ConfigMenu/ToggleFullscreenOption.cs
@@ -7,27 +7,17 @@ namespace SolStandard.HUD.Menu.Options.PauseMenu.ConfigMenu
 {
     public class ToggleFullscreenOption : MenuOption
     {
-        private bool isFullscreen;
         private readonly GameDriver gameDriver;
 
         public ToggleFullscreenOption(Color color, GameDriver gameDriver) : base(
             new RenderText(AssetManager.MainMenuFont, "Toggle Fullscreen"), color)
         {
-            isFullscreen = false;
             this.gameDriver = gameDriver;
         }
 
         public override void Execute()
         {
-            isFullscreen = !isFullscreen;
-            if (isFullscreen)
-            {
-                gameDriver.UseBorderlessFullscreen();
-            }
-            else
-            {
-                gameDriver.UseDefaultResolution();
-            }
+            gameDriver.ToggleFullscreen();
         }
 
         public override IRenderable Clone()
