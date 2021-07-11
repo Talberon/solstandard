f155358b3d9c810104a5c0c7e15f587e7ba3de68
Talberon <atheion@outlook.com>
2021-07-11 10:23:50 -0300

    Fixed bug where dead units would appear in the hover menu and would crash the game if a player attacked a breakable obstacle.

--- SolStandard/Utility/UnitSelector.cs
+++ SolStandard/Utility/UnitSelector.cs
@@ -8,7 +8,8 @@ namespace SolStandard.Utility
 {
     public static class UnitSelector
     {
-        public static GameUnit SelectUnit(MapEntity unit) => GlobalContext.Units.FirstOrDefault(gameUnit => gameUnit.UnitEntity == unit);
+        public static GameUnit SelectUnit(MapEntity unit) =>
+            GlobalContext.Units.FirstOrDefault(gameUnit => gameUnit.UnitEntity != null && gameUnit.UnitEntity == unit);
 
         public static UnitEntity FindOtherUnitEntityAtCoordinates(Vector2 coordinates, MapEntity excludedEntity)
         {
